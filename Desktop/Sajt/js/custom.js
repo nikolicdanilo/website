718b2b9c7d3954e69b6d7e91eb0299ff947633a4
nikolicdanilo <nikolic.danilo13@gmail.com>
2021-04-03 01:23:56 +0200

    inital

---
+++ Desktop/Sajt/js/custom.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function($){
+
+    setTimeout(function(){
+        $(".loader-wrapper").fadeOut();
+    },  1500);
+    
+
+    $("#search-btn").click(function(e){
+        e.preventDefault();
+        $(".search-wrapper").fadeIn();
+    });
+
+    $("#close-search").click(function(e){
+        e.preventDefault();
+        $(".search-wrapper").fadeOut();
+
+
+    });
+
+    $("#hamburger-btn").click(function(){
+       
+
+        if($(this).hasClass('opened')){
+            $("nav ul").slideUp();
+        }else{
+            $("nav ul").slideDown();
+        }
+        $(this).toggleClass('opened');
+        
+    });
+
+
+
+
+
+
+
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 100){
+
+            $("nav").addClass('fixed-nav');
+        }else{
+            $("nav").removeClass('fixed-nav');
+        }
+    });
+});
